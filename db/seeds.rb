c42601186a0f7c6e7612537c4b72c2e89e6a4091
Chris Boette <cboette@gmail.com>
2016-06-23 13:28:56 -0500

    Add code to seeds file to import from csv file into database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('data.csv'))
+
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+  t = Business.new
+  t.id = row['id']
+  t.uuid = row['uuid']
+  t.name = row['name']
+  t.address = row['address']
+  t.address2 = row['address2']
+  t.city = row['city']
+  t.state = row['state']
+  t.zip = row['zip']
+  t.country = row['country']
+  t.phone = row['phone']
+  t.website = row['website']
+  t.created_at = row['created_at']
+  t.save
+  puts "#{t.name} saved"
+end
+
+puts "There are now #{Business.count} rows in the businesses table"
