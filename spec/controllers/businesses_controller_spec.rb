faa83625abcc6600825640e916ed08517db1a17c
Chris Boette <cboette@gmail.com>
2016-06-23 23:17:00 -0500

    Add failing test for response length.

--- spec/controllers/businesses_controller_spec.rb
+++ spec/controllers/businesses_controller_spec.rb
@@ -6,5 +6,11 @@ describe BusinessesController do
       get :index, format: :json
       expect(response.headers['Content-Type']).to match 'application/json'
     end
+
+    it 'returns a maximum of fifty entries per page' do
+      51.times{ create(:business) }
+      get :index, format: :json
+      expect(JSON.parse(response.body).length).to be <= 50
+    end
   end
 end
