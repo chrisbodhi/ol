a8c81d119306c61bf2a818706acb8916dd652f3c
Chris Boette <cboette@gmail.com>
2016-06-25 13:18:16 -0500

    Scope the API as a module so that v1 is not in the URL.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
-    namespace :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :businesses, only: [:index, :show]
     end
   end
