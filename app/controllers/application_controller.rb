22a0bcccc0c42faa86c43661dd1955143ff062fd
Chris Boette <cboette@gmail.com>
2016-06-25 15:31:46 -0500

    Remove comments from database config file.
    Add custom 404 error message. Add test for error message, as well as page request that is outside the range of database entries.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+    def render_404
+      errorMessage = params[:id].nil? ? 'Error with your request -- sorry!' : "ID ##{params[:id]} not found in this database."
+      render :json => {
+        status: 404,
+        error: errorMessage
+      }, :status => 404
+    end
 end
