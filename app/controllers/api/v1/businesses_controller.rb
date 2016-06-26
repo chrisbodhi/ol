3926693ba6186ea1ecdfe0c22cec312c3f84cd1a
Chris Boette <cboette@gmail.com>
2016-06-26 15:03:24 -0500

    Add formatting option for JSON. Add instructions for its use to README.

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -1,18 +1,32 @@
+require 'json'
+
 module Api
   module V1
     class BusinessesController < ApplicationController
       def index
         @businesses = Business.paginate(:page => params[:page], :per_page => 50)
 
-        render json: @businesses
+        render_it(@businesses, params[:pretty])
       end
 
       def show
         @business = Business.find(params[:id])
-
-        render json: @business
+        render_it(@business, params[:pretty])
       end
 
+      private
+
+      def pretty_json(obj)
+        JSON.pretty_generate(JSON.parse(obj.to_json))
+      end
+
+      def render_it(obj, check)
+        if check
+          render json: pretty_json(obj)
+        else
+          render json: obj
+        end
+      end
     end
   end
 end
