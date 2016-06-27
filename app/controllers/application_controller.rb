c7b3597958d2e7ef2f28a3363d4ce37f3f22946d
Chris Boette <cboette@gmail.com>
2016-06-27 14:59:07 -0500

    Make 404 message structure a bit more semantically correct.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
     def render_404
-      errorMessage = params[:id].nil? ? 'Error with your request -- sorry!' : "ID ##{params[:id]} not found in this database."
       render :json => {
-        status: 404,
-        error: errorMessage
+        status: "404",
+        title: "ID Not Found",
+        detail: "Record with ID ##{params[:id]} not found in this database."
       }, :status => 404
     end
 end
